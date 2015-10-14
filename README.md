bdaa898f1f62bd84648c6eb44e5ecc212d72fad4
Ben Sadeghipour <bensadeghi@gmail.com>
2015-10-14 16:20:42 -0700

    v 1.2

--- README.md
+++ README.md
@@ -1,3 +1,47 @@
 # CMSFuzz
 Fuzzer for wordpress, cold fusion, drupal, joomla, and phpnuke.
 This project uses [secLists] (https://github.com/danielmiessler/SecLists/tree/master/Discovery/Web_Content/CMS)
+
+
+##Example output
+Please enter target URL (example: hackme.org)
+blog.example.com
+What are we fuzzing?
+          1:   Wordpress
+          2:   Cold Fusion
+          3:   Drupal
+          4:   Joomla
+          5:   PHP-Nuke
+          6:   Magento
+          7:   Sharepoint
+          8:   Common PHP Files
+          9:   Look for backup files
+          10:  Apache Default Files
+          11:  IIS Default Files
+          12:  Miscellaneous Files (SVN, robots, etc).
+          0:  Other
+
+Please enter an option:
+1
+fuzzing blog.example.com looking for Wordpress files.
+-----------------------------------------------------------------
+REDIRECTION AT: blog.example.com/wp-activate.php
+REDIRECTION AT: blog.example.com/wp-admin/
+200 FOUND AT: blog.example.com/wp-admin/admin-ajax.php
+200 FOUND AT: blog.example.com/wp-admin/admin-footer.php
+REDIRECTION AT: blog.example.com/wp-admin/admin.php
+200 FOUND AT: blog.example.com/wp-admin/admin-post.php
+REDIRECTION AT: blog.example.com/wp-admin/async-upload.php
+REDIRECTION AT: blog.example.com/wp-admin/comment.php
+FORBIDDEN AT: blog.example.com/wp-admin/css/
+200 FOUND AT: blog.example.com/wp-admin/css/dashboard.css
+200 FOUND AT: blog.example.com/wp-admin/css/dashboard-rtl.css
+200 FOUND AT: blog.example.com/wp-admin/css/farbtastic.css
+200 FOUND AT: blog.example.com/wp-admin/css/farbtastic-rtl.css
+200 FOUND AT: blog.example.com/wp-admin/css/ie.css
+200 FOUND AT: blog.example.com/wp-admin/css/ie-rtl.css
+200 FOUND AT: blog.example.com/wp-admin/css/install.css
+200 FOUND AT: blog.example.com/wp-admin/css/install-rtl.css
+200 FOUND AT: blog.example.com/wp-admin/css/login.css
+200 FOUND AT: blog.example.com/wp-admin/css/login-rtl.css
+200 FOUND AT: blog.example.com/wp-admin/css/media.css
--- dirbf.rb
+++ dirbf.rb
@@ -11,17 +11,45 @@ require 'open-uri'
 #Written By Behrouz Sadeghipour
 
 
+class String
+def black;          "\e[30m#{self}\e[0m" end
+def red;            "\e[31m#{self}\e[0m" end
+def green;          "\e[32m#{self}\e[0m" end
+def brown;          "\e[33m#{self}\e[0m" end
+def blue;           "\e[34m#{self}\e[0m" end
+def magenta;        "\e[35m#{self}\e[0m" end
+def cyan;           "\e[36m#{self}\e[0m" end
+def brown;           "\e[37m#{self}\e[0m" end
+def gray;           "\e[37m#{self}\e[0m" end
+
+def bg_black;       "\e[40m#{self}\e[0m" end
+def bg_red;         "\e[41m#{self}\e[0m" end
+def bg_green;       "\e[42m#{self}\e[0m" end
+def bg_brown;       "\e[43m#{self}\e[0m" end
+def bg_blue;        "\e[44m#{self}\e[0m" end
+def bg_magenta;     "\e[45m#{self}\e[0m" end
+def bg_cyan;        "\e[46m#{self}\e[0m" end
+def bg_brown;        "\e[47m#{self}\e[0m" end
+
+def bold;           "\e[1m#{self}\e[22m" end
+def italic;         "\e[3m#{self}\e[23m" end
+def underline;      "\e[4m#{self}\e[24m" end
+def blink;          "\e[5m#{self}\e[25m" end
+def reverse_color;  "\e[7m#{self}\e[27m" end
+end
+
+system "clear"
 
 def sor_res_code(res, targetURI)
   case res.code
     when "401"
-      puts "PROTECTED DIR AT:" + targetURI
+      print ("PROTECTED DIR AT:" + targetURI).gray
     when "200"
-      puts "200 FOUND AT: " + targetURI
+      print ("200 FOUND AT: " + targetURI).green
     when "403"
-      puts "FORBIDDEN AT: " + targetURI
+      print ("FORBIDDEN AT: " + targetURI).red
     when "302"
-      puts "REDIRECTION AT: " + targetURI
+      print ("REDIRECTION AT: " + targetURI).gray
     end
 end
 
@@ -35,7 +63,6 @@ def get_response_code(targetURI)
       sor_res_code res, targetURI
     end
   rescue URI::InvalidURIError
-    puts "InvalidURIError"
   end
 end
 
@@ -119,7 +146,6 @@ else
 end
 
 if File.file?(file_name)
-  puts "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
   puts "fuzzing " + getURI + " looking for " + fuzz_name + " files."
   puts "-----------------------------------------------------------------\n"
   openFile file_name, getURI
